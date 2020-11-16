48f1bb2804eecbc5d8fe8e4fe11692e4c331d073
Adam Averay <adam@averay.com>
2020-11-16 12:02:13 +0900

    Enforce strict types

--- src/AdamAveray/SalesforceUtils/Client/ClientInterface.php
+++ src/AdamAveray/SalesforceUtils/Client/ClientInterface.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace AdamAveray\SalesforceUtils\Client;
 
 use AdamAveray\SalesforceUtils\Queries\QueryInterface;
--- src/AdamAveray/SalesforceUtils/Data/Picklist.php
+++ src/AdamAveray/SalesforceUtils/Data/Picklist.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace AdamAveray\SalesforceUtils\Data;
 
 class Picklist
